3f18edb1f199d668e6934c55a8a1dc028a5f1697
Amina Saeed <aminasaeed223@gmail.com>
2025-07-27 07:15:17 +0500

    commit

---
+++ lms-app/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      <h1 className="text-2xl">Welcome to my SaaS app</h1>
+      <Button>
+        Submit
+      </Button>
+    </>
+  );
+}
+
+export default Page
